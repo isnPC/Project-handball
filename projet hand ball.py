0d31afc553032dc23e7c71fc46ae1e6ce4abf7da
guillaume-to <60393102+guillaume-to@users.noreply.github.com>
2020-03-09 22:44:04 +0100

    Add files via upload
    liste des fonction à faire peut changer au cour du temp

---
+++ projet hand ball.py
@@ -0,0 +1,38 @@
+﻿def affichageterrain():
+    """affiche le terrain"""
+
+def selectionjoueur():
+    """demande le nom des joueurs et les mets dans une liste"""
+
+def affichagejoueur():
+    """affiche la liste des joueur"""
+
+def selectionjoueur():
+    """recupere le nom du joueur séléctionner et le met dans une variable"""
+
+def affichageaction():
+    """affiche la liste d'action"""
+def selectionaction():
+    """recupere l'action séléctionner et le met dans une variable"""
+def affichagelouperreussi():
+    """affiche l'option reussit ou louper"""
+def selectionlouperreussi():
+    """recupere si l'action est reussit ou louper et le met dans une variable"""
+def affichageresultat():
+    """affiche un bouton résultat"""
+def selectionresultat():
+    """recupere si l'utilisateur appuye sur résultat et le met dans une variable"""
+def emplacementdonner():
+    """recupere l'endroit où l'utilisateur à cliquer et le stocke dans une variable"""
+def enregistrerfichier():
+    """stocke les information dans un fichier excel
+    Entrée: nom du joueur, action, louper/réussi et la position
+    Sortie: stocke toute les information dans un fichier excel"""
+def calculestat():
+    """recupere le nombre de reussi et de gagner et calcule le pourcentage de réussite """
+def affichageposition():
+    """Entrée:action séléctionné, joueur selectionné, tout les emplacement de l'action et si elles sont reussit ou non
+    Affiche les emplacement de toute les actions en vert si elle est réussi et rouge si elle est loupé pour un joueur et une action donné"""
+def affichagestat():
+     """Entreée: le pourcentage de réussite, le nombre d'action réussit et louper a partir du fichier exel et le joueur séléctionner
+     Affiche le pourcentage de réussite à coté de chaque action et le nombre de réussite-le nombre d'echecs acoté du pourcentage """
